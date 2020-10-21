1b42c3a5dda58c5568bc2ea20785a0b188ab3f5f
Mikhail Nagorniy <mustepvbif@yandex.ru>
2020-10-21 22:58:29 +0300

    code upd

--- venv/Include/main.py
+++ venv/Include/main.py
@@ -2,31 +2,21 @@ from aiogram import executor
 from tortoise import Tortoise, run_async
 from loguru import logger
 from config import *
-from fastapi import FastAPI
-from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 import core.handlers
 import core.helpers
 import asyncio
-import uvicorn
 
 
-app = FastAPI(
-    title="FondLeaderBot"
-)
-
-
-register_tortoise(
-    app,
-    db_url="sqlite://tokens.sqlite3",
-    modules={"models": ["models"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
-executor.start_polling(dp, skip_updates=True)
-
+async def main():
+    logger.info("Tortoise inited!")
+    await Tortoise.init(
+        db_url="sqlite://tokens.sqlite3",
+        modules={"models": ["models"]}
+    )
+    await Tortoise.generate_schemas()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",
-                debug=True)
+    run_async(main())
+    executor.start_polling(dp, skip_updates=True)
 
 
